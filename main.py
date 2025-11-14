a4bdba88153d416ed8b7e207952a890e1e25a1d5
Aditya Giri Goswami <adityagiri@gkmit.co>
2025-11-14 23:22:52 +0530

    fix: status code in main.py & modify .env.example

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-from fastapi import FastAPI
+from fastapi import FastAPI ,status
 from db.config import engine, Base
 
 app = FastAPI(title="Asset Allocation System")
 
-@app.get("/")
+@app.get("/",status_code=status.HTTP_200_OK)
 def root():
-    return {"message": "Welcome to Asset Allocation System API"}
+    return { "status_code" :status.HTTP_200_OK ,"message": "Welcome to Asset Allocation System API"}
